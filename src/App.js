86697085b7b67e178a31add8f65316568ab29628
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2021-03-08 10:37:54 +0200

    Mapping and handling of tags updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import ImageCard from "./Components/ImageCard";
+import ImageSearch from "./Components/ImageSearch";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [searchString, setSearchString] = useState("");
 
   const url = "https://pixabay.com/api/?key=17949228-be8f0aed3c84bfb102ac75e40";
 
-  const fetchData = async () => {
-    let rawData = await fetch(url);
-    let data = await rawData.json();
-    setData(data);
-    setLoading(false);
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      let updatedUrl = url;
+      if (searchString !== "") {
+        updatedUrl = updatedUrl + "&q=" + searchString;
+      }
+      let rawData = await fetch(updatedUrl);
+      let data = await rawData.json();
+      setData(data);
+      setLoading(false);
+    };
+
     fetchData();
-  }, []);
+    return () => {};
+  }, [searchString]);
 
   return (
     <div className="App">
-      <h2>You have clicked {count} times</h2>
-      <button onClick={() => setCount(count + 1)}>click me</button>
-      {loading ? "" : <div>{data.hits[0].id}</div>}
+      <ImageSearch setSearchText={setSearchString} />
+      {loading ? "" : data.hits.map((item) => <ImageCard item={item} />)}
     </div>
   );
 }
--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -1,5 +1,29 @@
 import React from "react";
 
-export const ImageCard = () => {
-  return <div>What am I doing</div>;
+const ImageCard = ({ item }) => {
+  const tags = item.tags.split(",");
+
+  const maxTagLength = (item) => {
+    if (item.length < 10) {
+      return `${item}`;
+    } else {
+      console.log(item);
+    }
+  };
+
+  return (
+    <div>
+      <img src={item.webformatURL} alt="" />
+      <div>{item.user}</div>
+      <div>{item.views}</div>
+      <div>{item.likes}</div>
+      <div>
+        {tags.map((tag, index) => (
+          <span key={index}>{maxTagLength(tag)}</span>
+        ))}
+      </div>
+    </div>
+  );
 };
+
+export default ImageCard;

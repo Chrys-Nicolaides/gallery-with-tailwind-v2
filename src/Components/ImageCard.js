2a11eb462c0ff746db4c4e708c3ede0d21fe454a
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2021-03-15 23:26:22 +0200

    tailwind dark mode enabled & theme styling updated

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -18,25 +18,33 @@ const ImageCard = ({ item }) => {
   };
 
   return (
-    <div className="max-w-xs rounded-3xl overflow-hidden shadow-md bg-gradient-to-b from-gray-700 to-gray-800">
+    <div className="max-w-xs rounded-3xl overflow-hidden shadow-md bg-gradient-to-b from-purple-50 to-purple-100 dark:bg-gradient-to-b dark:from-gray-700 dark:to-gray-800">
       <img src={item.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-medium text-gray-500 text-sm mt-2">Photo by</div>
-        <div className="font-bold text-gray-100 text-2xl mb-8">{item.user}</div>
-        <div className="border-b-2 border-gray-600 mb-8"></div>
+        <div className="font-bold text-gray-800 dark:text-gray-100 text-2xl mb-8">
+          {item.user}
+        </div>
+        <div className="border-b-2 border-gray-300 dark:border-gray-600 mb-8"></div>
         <ul className="text-sm flex space-x-9">
-          <div className="text-gray-500">
+          <div className="">
             <li className="flex items-center">
-              <AiFillEye className="text-gray-300" size={"1.5em"} />
-              <strong className="text-gray-400 font-medium ml-1.5 ">
+              <AiFillEye
+                className="text-gray-500 dark:text-gray-300"
+                size={"1.5em"}
+              />
+              <strong className="text-gray-500 dark:text-gray-300 font-medium ml-1.5 ">
                 {item.views}
               </strong>
             </li>
           </div>
-          <div className="-ml-20 text-gray-500">
+          <div className="-ml-20">
             <li className="flex items-center">
-              <AiFillHeart className="text-gray-300" size={"1.5em"} />
-              <strong className="text-gray-400 font-medium ml-1.5">
+              <AiFillHeart
+                className="text-gray-500 dark:text-gray-300"
+                size={"1.5em"}
+              />
+              <strong className="text-gray-500 dark:text-gray-300 font-medium ml-1.5">
                 {item.likes}
               </strong>
             </li>
--- src/Components/ImageSearch.js
+++ src/Components/ImageSearch.js
@@ -17,15 +17,14 @@ const ImageSearch = (props) => {
 
   return (
     <div>
-      <h2
-        className="text-gray-700 text-opacity-50 text-10xl font-bold -my-10 -ml-16 text-left min-w-max z-50 overflow-visible
-      "
-      >
-        Media Gallery
-      </h2>
+      <div>
+        <h2 className="text-purple-200 dark:text-gray-700 text-opacity-50 text-10xl font-bold -my-10 -ml-16 text-left min-w-max z-50 overflow-visible">
+          Media Gallery
+        </h2>
+      </div>
       <div className="flex justify-center flex-nowrap align-center self-center -mt-28">
         <div className="linear-gradient flex justify-center h-24 min-w-min mb-4 px-1">
-          <h1 className="text-white flex justify-center text-8xl font-bold text-center opacity-100 -mt-1">
+          <h1 className="text-gray-700 dark:text-white flex justify-center text-8xl font-bold text-center opacity-100 -mt-1">
             Media Gallery.
           </h1>
         </div>
@@ -33,14 +32,14 @@ const ImageSearch = (props) => {
       <div className="mt-24 mb-20 grid grid-cols-3 gap-4">
         <form
           onSubmit={submitHandler}
-          className="col-start-2 mx-7 border-4 border-gray-600 rounded-full hover:border-gray-500 hover:bg-gray-800 flex justify-around h-14"
+          className="col-start-2 mx-7 border-4 border-purple-300 hover:border-purple-400 hover:bg-purple-100 focus:bg-purple-100 dark:border-gray-600 rounded-full dark:hover:border-gray-500 dark:hover:bg-gray-800 flex justify-around h-14"
         >
           <input
             type="text"
             placeholder="Search images here"
             value={inputText}
             onChange={(event) => changeHandler(event)}
-            className="ml-8 text-base placeholder-gray-400 hover:text-gray-300 text-gray-300 appearance-none bg-transparent border-none w-full font-medium mr-3 py-1 px-2 leading-tight focus:outline-none"
+            className="ml-8 text-base placeholder-gray-600 dark:placeholder-gray-400 text-gray-800 dark:text-gray-300 appearance-none bg-transparent border-none w-full font-medium mr-3 py-1 px-2 leading-tight focus:outline-none"
           />
           <div className="flex align-center mx-8 mt-1">
             <button
@@ -48,7 +47,7 @@ const ImageSearch = (props) => {
               className="outline-none focus:outline-none"
             >
               <IoSearch
-                className="flex align-center items-center text-gray-500 hover:text-gray-300"
+                className="flex align-center items-center text-gray-600 hover:text-gray-800 dark:text-gray-500 dark:hover:text-gray-300"
                 size={"1.4em"}
                 strokeWidth={"1.2em"}
               />
